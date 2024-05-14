c30f748fa734ec3ce58ae8f444f26583820e0093
Henry <mbahenryemeka@gmail.com>
2024-05-13 23:13:34 -0400

    the connect function to handle incoming data and console.log it for the player.

---
+++ play.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541,
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data)
+  });
+
+  conn.setEncoding('utf8');
+  return conn;
+};
+
+console.log('connecting .....');
+connect();
